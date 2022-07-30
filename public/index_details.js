6fc2bcb9a264978cd8673f781e320f8bc0a54d63
Shoaib Ansari <66304849+shoaib749@users.noreply.github.com>
2022-07-30 20:44:03 +0530

    Temp

--- public/index_details.js
+++ public/index_details.js
@@ -7,14 +7,14 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAG5iICEez2P75v-hAWJb4wPBfB-CP15OI",
-  authDomain: "dums-d3398.firebaseapp.com",
-  databaseURL: "https://dums-d3398-default-rtdb.firebaseio.com",
-  projectId: "dums-d3398",
-  storageBucket: "dums-d3398.appspot.com",
-  messagingSenderId: "977563357836",
-  appId: "1:977563357836:web:bf8047ba5045f9fe99a1a9",
-  measurementId: "G-SKFSHV7HP1"
+     apiKey: "AIzaSyAG5iICEez2P75v-hAWJb4wPBfB-CP15OI",
+     authDomain: "dums-d3398.firebaseapp.com",
+     databaseURL: "https://dums-d3398-default-rtdb.firebaseio.com",
+     projectId: "dums-d3398",
+     storageBucket: "dums-d3398.appspot.com",
+     messagingSenderId: "977563357836",
+     appId: "1:977563357836:web:bf8047ba5045f9fe99a1a9",
+     measurementId: "G-SKFSHV7HP1"
 };
 
 // Initialize Firebase
@@ -23,8 +23,11 @@ const analytics = getAnalytics(app);
 //firebase 
 import { getDatabase, ref, set, child, get } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-database.js"
 const db = getDatabase();
-
-
+//storage
+import { getStorage, ref as sRef, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-storage.js";
+var files = [];
+var pic = [];
+var reader = new FileReader();
 const name = document.getElementById("nameInput");
 const gender = document.getElementById("");
 const dob = document.getElementById("dobInput");
@@ -36,24 +39,137 @@ const guardianName = document.getElementById("guardianNameInput");
 const guardianPhone = document.getElementById("guardianPhoneInput");
 const guardianEmail = document.getElementById("guardianEmailInput");
 const submit = document.getElementById("save");
-submit.addEventListener('click',generate);
-function  test(){
-     console.log(localStorage.getItem("username"));  
+
+
+//
+const imgProfile = document.getElementById("proPicholder");
+const B_selct_profile = document.getElementById("selPropic");
+//buttons for chosing 
+const B_select = document.getElementById("selectButton");
+const DocName = document.getElementById("fileName");
+
+B_select.onclick = function () {
+     input.click();
 }
+submit.onclick = function () {
+     UploadProcess();
+     window.location = "showUserDetails.html";
+}
+var input = document.createElement('input');
+input.type = 'file';
+input.onchange = e => {
+     files = e.target.files;
+     var extension = GetFileExt(files[0]);
+     var name = GetFileName(files[0]);
+     DocName.innerHTML = name;
+
+}
+
 //genrating QR code 
-function generate(){   
-     var data =email.value;
+function  generate() {
+     var data = email.value;
      console.log(data);
      var url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}`;
-     // code.src = url;
      console.log(url);
-     // console.log(code.src);
- }
+     return url.toString();
+}
+async function downloadImage(imageSrc) {
+     const image = await fetch(imageSrc)
+     const imageBlog = await image.blob()
+     const imageURL = URL.createObjectURL(imageBlog)
+   
+     const link = document.createElement('a')
+     link.href = imageURL
+     link.download = localStorage.getItem("username");
+     document.body.appendChild(link)
+     link.click()
+     document.body.removeChild(link)
+   }
 
-//function for sending data to firebase 
-function sendDataDB(){
-     const dbRef = ref(db);
-     get(child(dbRef,"PatientData"))
+//testing for async upload function
+async function UploadProcess() {
+     var Doc = files[0];
+     var Name = DocName.innerHTML;
+     const metaData = {
+          contentType: Doc.type
+     }
+
+     const storage = getStorage();
+     const storageRef = sRef(storage, localStorage.getItem("username") + "/PatientDoc/" + Name);
+     const UploadTask = uploadBytesResumable(storageRef, Doc, metaData);
+     UploadTask.on('state-change', (snapshot) => {
+          //     var progress_spinner = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          //     progress.innerHTML = "Upload " + progress_spinner + "%";
+     },
+          (error) => {
+               alert("Error in upload");
+          },
+          () => {
+               getDownloadURL(UploadTask.snapshot.ref)
+                    .then((downloadURL) => {
+                         console.log(downloadURL);
+                         saveUrlToDB(downloadURL);
+                    });
+          });
+}
+function saveUrlToDB(link) {
+     var D_name = name.value;
+    // var D_gender = gender.value;
+     var D_dob = dob.value;
+     var D_email = email.value;
+     var D_adhar = adhar.value;
+     var D_bloodgroup = bloodgroup.value;
+     var D_guardianName = guardianName.value;
+     var D_guardianPhone = guardianPhone.value;
+     var D_guardianEmail = guardianEmail.value;
+     var D_phoneNo = phone.value;
+
+     set(ref(db, "PatientData/" + localStorage.getItem("username")), {
+          Name: D_name,
+         // Gender : D_gender,
+          Email : D_email,
+          DOB : D_dob,
+          Addhar : D_adhar,
+          Bloodgroup : D_bloodgroup,
+          GuardianEmail : D_guardianEmail,
+          GuardianPhone:D_guardianPhone,
+          guardianName:D_guardianName,
+          PhoneNo:D_phoneNo,
+          QRlink: generate(),
+          DocLink: link
+     });
 }
 
+
+function GetFileExt(file) {
+     var temp = file.name.split('.');
+     var ext = temp.slice((temp.length - 1), (temp.length));
+     return '.' + ext[0];
+}
+function GetFileName(file) {
+     var temp = file.name.split('.');
+     var fname = temp.slice(0, -1).join('.');
+     console.log(fname);
+     return fname;
+}
+
+//for profile picture
+var input_2 = document.createElement('input');
+input_2.type = 'file';
+input_2.onchange = e => {
+    pic = e.target.pic;
+//     var extension = GetFileExt(files[0]);
+//     var name = GetFileName(files[0]);
+//     namebox.value = name;
+//     ext.innerHTML = extension;
+    reader.readAsDataURL(pic[0]);
+    console.log(pic[0]);
+}
+reader.onload = function () {
+//     imgProfile.setAttribute('src',reader.result);
+      imgProfile.src = reader.result;
+}
+B_selct_profile.onclick = function(){
+     input_2.click();
+}
 
