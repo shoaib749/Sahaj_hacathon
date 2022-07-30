9d47fdcb593836c87c774f8ae3ee881402521dc9
Shoaib Ansari <66304849+shoaib749@users.noreply.github.com>
2022-07-30 13:09:57 +0530

    Revert "Create index.js"
    This reverts commit f97597db2b79a52ddef6af681704057e63548516.

---
+++ index.js
@@ -0,0 +1,31 @@
+//import statements for firebase 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.1/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAG5iICEez2P75v-hAWJb4wPBfB-CP15OI",
+  authDomain: "dums-d3398.firebaseapp.com",
+  databaseURL: "https://dums-d3398-default-rtdb.firebaseio.com",
+  projectId: "dums-d3398",
+  storageBucket: "dums-d3398.appspot.com",
+  messagingSenderId: "977563357836",
+  appId: "1:977563357836:web:bf8047ba5045f9fe99a1a9",
+  measurementId: "G-SKFSHV7HP1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//functions for signUppage
+//init for signup page
+const fullName = document.getElementById("");
+
+
+
+
+
+
+
+//functions for login page
+//init for login page
